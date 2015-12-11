efb950f045b13bd17615ef20de26abf865869046
Andre Josefsson <andre.v.json@gmail.com>
2015-12-11 12:19:25 +0100

    Adds routes.js for routing.

--- server.js
+++ server.js
@@ -5,14 +5,15 @@
 var express = require('express');
 var app = express();
 
-//Traffic routing.
+//Paths and routing.
 var port = 8080;
+require('./backend/routes.js')(app);
 app.use(express.static(__dirname + '/public'));
 app.use('/angular', express.static(__dirname + '/node_modules/angular'));
 app.use('/angular-animate', express.static(__dirname + '/node_modules/angular-animate'));
 app.use('/angular-aria', express.static(__dirname + '/node_modules/angular-aria'));
 app.use('/angular-route', express.static(__dirname + '/node_modules/angular-route'));
 app.use('/angular-material-icons', express.static(__dirname + '/node_modules/angular-material-icons'));
-app.listen(port);
 
+app.listen(port);
 console.log("Server is now listening on port: " + port);
