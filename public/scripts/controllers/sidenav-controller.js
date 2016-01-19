a311cd735017a0a8d9c4963dd4ebf2190f94c796
Andre Josefsson <andre.v.json@gmail.com>
2016-01-19 23:04:43 +0100

    Adds tracking of the hash for displaying current path and adds listing of the possible subpages to the sideNav.

--- public/scripts/controllers/sidenav-controller.js
+++ public/scripts/controllers/sidenav-controller.js
@@ -3,6 +3,12 @@
 angular.module('andrejson')
 	.controller('sidenavController', function ($scope, $log, $mdSidenav) {
 		'use strict';
+	
+		$scope.views = [
+			'about-me',
+			'home'
+		];
+	
 		$scope.closeLeft = function () {
 			$mdSidenav('left').close();
 		};
--- public/scripts/controllers/toolbar-controller.js
+++ public/scripts/controllers/toolbar-controller.js
@@ -1,8 +1,19 @@
 /*global angular*/
 
 angular.module('andrejson')
-	.controller('toolbarController', function ($scope, $log, $mdSidenav) {
+	.controller('toolbarController', function ($scope, $location, $log, $mdSidenav) {
 		'use strict';
+	
+		$scope.getPath = function () {
+			var path, firstLetter;
+			path = $location.path().substr(1);
+			path = path.replace('-', ' ');
+			firstLetter = path.substring(0, 1);
+			firstLetter = firstLetter.toUpperCase();
+			path = path.slice(1);
+			return firstLetter + path;
+		};
+	
 		$scope.openLeft = function () {
 			$mdSidenav('left').open();
 		};
