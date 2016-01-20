965b7a2ce9026fac6ee99005378bfd164a1e18ac
Andre Josefsson <andre.v.json@gmail.com>
2016-01-20 21:29:52 +0100

    Adds some content to the about page.

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use('/angular-route', express.static(__dirname + '/node_modules/angular-rout
 app.use('/angular-material', express.static(__dirname + '/node_modules/angular-material'));
 app.use('/angular-material-icons', express.static(__dirname + '/node_modules/angular-material-icons'));
 app.use('/stylesheets', express.static(__dirname + '/css/stylesheets'));
+app.use('/images', express.static(__dirname + '/images'));
 
 app.listen(port);
 console.log("Server is now listening on port: " + port);
